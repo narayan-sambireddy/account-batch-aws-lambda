a58ecfc61547db419f8fb1aa5c1a111ffb59a4dc
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-06-24 07:07:23 +0530

    Initial commit

---
+++ src/main/java/smartbank/services/accountbatch/AccountBatchAwsLambdaApplication.java
@@ -0,0 +1,12 @@
+package smartbank.services.accountbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBatchAwsLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountBatchAwsLambdaApplication.class, args);
+	}
+}
